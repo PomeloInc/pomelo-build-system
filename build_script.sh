a0b289ef68b7bc55b51a85893df61136b77ee0a4
pomelo <ben.humml.tech@gmail.com>
2022-05-07 21:59:46 +0000

    Added arguments to script

--- build_script.sh
+++ build_script.sh
@@ -1,15 +1,44 @@
 #!/bin/bash
 
-var="Hello World"
+set -e
 
-cd /workdir/yocto
-# source poky/oe-init-build-env /board/$1/
-source poky/oe-init-build-env /workdir/yocto/board/bbb/
+usage="$(basename "$0") [-h] [-b BOARD] [-r RECIPE] [-c YOCTO_CMD]
+Build a yocto project
+where:
+    -h  show this help text
+    -b  the board you want to build
+    -r  the recipe you want to build
+    -c  a yocto-project based command you want to execute instead (e.g. devtool)"
 
+# constants
+YOCTO=/workdir/yocto
+
+options=':h:b:r:c:'
+while getopts $options option; do
+  case "$option" in
+    h) echo "$usage"; exit;;
+    b) BOARD=$OPTARG;;
+    r) RECIPE=$OPTARG;;
+    c) YOCTO_CMD=$OPTARG;;
+    :) printf "missing argument for -%s\n" "$OPTARG" >&2; echo "$usage" >&2; exit 1;;
+   \?) printf "illegal option: -%s\n" "$OPTARG" >&2; echo "$usage" >&2; exit 1;;
+  esac
+done
+
+# mandatory arguments
+if [ ! "$BOARD" && ! "$RECIPE"] || [ ! "$YOCTO_CMD" ]; then
+  echo "arguments -b and -r must be provided"
+  echo "$usage" >&2; exit 1
+fi
+
+# source poky/oe-init-build-env
+source $YOCTO/poky/oe-init-build-env $YOCTO/board/bbb/
+# bitbake core-image-minimal
  
-# print it 
-echo "$var"
-
-echo "BUILD=$1"
+# DEBUG
+echo "Hello World"
+echo "BOARD=$1"
 echo "RECIPE$2"
+echo "BOARD=$BOARD"
+echo "RECIPE=$RECIPE"
 
